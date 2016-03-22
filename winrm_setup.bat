40dabeab442775f3f25554ea49e7079a02d49138
883 <000014h@gmail.com>
2016-03-22 13:53:04 +0900

    winrm script modified.

--- winrm_setup.bat
+++ winrm_setup.bat
@@ -4,6 +4,7 @@ cmd.exe /c winrm quickconfig -q
 cmd.exe /c winrm quickconfig -transport:http
 cmd.exe /c winrm set winrm/config @{MaxTimeoutms="1800000"}
 cmd.exe /c winrm set winrm/config/winrs @{MaxMemoryPerShellMB="300"}
+cmd.exe /c winrm set winrm/config/winrs @{AllowRemoteShellAccess="true"}
 cmd.exe /c winrm set winrm/config/service @{AllowUnencrypted="true"}
 cmd.exe /c net stop winrm 
 cmd.exe /c sc config winrm start=auto
